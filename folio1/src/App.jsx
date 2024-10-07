4f90bd1a49a13c87ee67561c593d1a434824e547
Sanket <sanketd2104@gmail.com>
2024-10-08 00:31:58 +0530

    splash

--- folio1/src/App.jsx
+++ folio1/src/App.jsx
@@ -2,13 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import GradientText from './components/GradientText'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Sanket Dhumal</h1>
+      <GradientText
+        colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]} // Custom gradient colors
+        animationSpeed={3} // Custom animation speed in seconds
+        showBorder={false} // Show or hide border
+        className="custom-class" // Add one or more custom classes
+      >
+        <h1>Sanket Dhumal</h1>
+      </GradientText>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -20,6 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      
     </>
   )
 }
